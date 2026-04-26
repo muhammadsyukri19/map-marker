9c4eee91b7c005a85d4a55dbb026ce38c77d61e8
MuhammadAzaniIrvand <muhammad.azaniirvand@gmail.com>
2026-04-26 23:51:46 +0700

    feat: menambahkan marker

---
+++ kelompok-9/marker.js
@@ -0,0 +1,7 @@
+const data_titik = [
+  ["Blang Padang", 5.5512, 95.3143, "Lapangan bekas pangkalan udara Belanda, jadi ruang publik warga Banda Aceh"],
+  ["Masjid Baiturrahim", 5.5698, 95.2889, "Masjid yang tetap berdiri kokoh saat tsunami 2004 menerjang"],
+  ["Kerkhoff Peucut", 5.5521, 95.3168, "Kompleks pemakaman serdadu Belanda yang gugur dalam perang Aceh"],
+  ["Rumah Cut Nyak Dhien", 5.5489, 95.3221, "Kediaman pahlawan nasional Cut Nyak Dhien di tengah kota"],
+  ["Bustanussalatin", 5.5548, 95.3201, "Taman kerajaan terluas peninggalan masa Kesultanan Aceh Darussalam"],
+];
